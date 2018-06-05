90ce1663aefdbb415d0b1b87a64c79c4eecae4af
jarturoch <jorgechav@gmail.com>
2018-06-04 23:42:19 -0400

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,12 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
     date = models.DateTimeField()
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:150]
+
+    def pub_date_pretty(self):
+        return self.date.strftime('%b %e %Y')
